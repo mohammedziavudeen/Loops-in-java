ba67f2257b4aab9ec23a501b9be97c7c34e7b1c2
mohammedziavudeen <123461410+mohammedziavudeen@users.noreply.github.com>
2023-02-12 21:07:15 +0530

    Update Milk.java

--- Milk.java
+++ Milk.java
@@ -1,9 +1,10 @@
+//this program to find total amount of milk,whereas buying alternative days
 public class Milk {
 
 	public static void main(String[] args) {
 		int day = 1,sum=0;
 		while(day<=30&&day%2==1) {
-			sum = sum + 21;
+			sum = sum + 21; //21 is Half litre milk rate
 			day = day+2;
 		}
 		System.out.println(sum);
